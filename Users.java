831ebb1ab08826c2654140eedf82fe7cae9d7b6b
unknown <sambotelho07@gmail.com>
2025-05-16 17:35:39 -0300

    users model

--- Users.java
+++ Users.java
@@ -2,6 +2,37 @@ public class Users{
     String nome;
     String username;
     String email;
+    String senha;
+    float saldo;
     int iD;
     String nDeCelular;
+
+    public Users(String user, String password ){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public int getiD() {
+        return iD;
+    }
+
+    public String getnDeCelular() {
+        return nDeCelular;
+    }
+
 }
