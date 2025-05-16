0715a135e040b3372499da54961f7f374b6cbff8
unknown <sambotelho07@gmail.com>
2025-05-16 17:11:13 -0300

    .gitignore add

--- Hud.java
+++ Hud.java
@@ -1,3 +1,15 @@
 public class Hud {
+    public static void menu(){
+        System.out.println("""
+                Qual operação você ira fazer?
+                
+                1 - Consultar saldos
+                2 - Receber valor
+                3 - Transferir valor
+                4 - sair
+                
+                Digite a opção desejada:
+                """);
+    }
 
 }
